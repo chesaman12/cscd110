33dd2d3dd5173cfaeba23d81dd0f19e0268566e0
Nathaniel Elliott <Nathaniel Elliott>
2015-11-29 16:52:53 -0800

    wordplay.py

--- Wordplay.py
+++ Wordplay.py
@@ -1,7 +1,8 @@
 def main():
 
+
+
     looping()
-    
 
 def print_it():
     print('''
@@ -14,7 +15,20 @@ def print_it():
 
      ''')
 
+def openfile():
+    f = open('file.txt', 'r')
+    filedata = f.readlines()
+    f.close()
+    i = 0
+    for j in filedata:
+        filedata[i] = j.strip()
+        i += 1
+    aDict = { 'file' : filedata }
+    return aDict
+
 def looping():
+    print_it()
+    dictionary = openfile()
     while True:
         ui = userInput()
         if ui == 1:
@@ -22,21 +36,23 @@ def looping():
         #elif ui == 2:
         #elif ui == 3:
         elif ui == 4:
-            is_palindrome()
+            userinput = int(input("Enter length of palindrome you would like to enter: "))
+            for word in dictionary:
+                is_palindrome(word)
         elif ui == 0:
             break
 
 def  userInput():
-    print_it()
     ui = int(input("What option would you like? "))
     return ui
 
-def is_anagram():
-    return  True
-
-def is_palindrome():
+def is_anagram(str1, str2):
 
 
-
+def is_palindrome(word):
+        if len(word) == ui and word == word[::-1]:
+            return True
+        else:
+            return print("not valid")
 
 main()
